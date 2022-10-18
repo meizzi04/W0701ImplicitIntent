0a6e434ca40611d8427b29474473e5c2e151118f
DESKTOP-IV3OEM3\alfud <alfud9078@naver.com>
2022-10-18 14:43:21 +0900

    ✨ MAP, SMS Button 추가

--- app/src/main/java/kr/ac/kumoh/s20200158/w0701implicitintent/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20200158/w0701implicitintent/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kr.ac.kumoh.s20200158.w0701implicitintent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +18,32 @@ class MainActivity : AppCompatActivity() {
         binding.btnHomepage.setOnClickListener{
             val uri = Uri.parse("http://www.instagram.com/meizzi_2/")//url(위치 지정)과 uri(이름 정할 떄 사용)
             val intent = Intent(Intent.ACTION_VIEW, uri)
+
             startActivity(intent)
+        }
+
+        binding.btnMap.setOnClickListener{
+            val uri = Uri.parse("geo : 0,0?z=17&q=충남 금산군 군청 5길 25")//geo : 36.145014,128.393047?z=17 //geo : 0,0?z=17&q=주소
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            if(intent.resolveActivity(packageManager)!=null)
+                startActivity(intent)
+            else
+            {
+                Toast.makeText(this, "연결할 앱이 없습니다",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.btnSms.setOnClickListener{
+            val uri = Uri.parse("sms : 010-4165-9078")//url(위치 지정)과 uri(이름 정할 떄 사용)
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            if(intent.resolveActivity(packageManager)!=null)
+                startActivity(intent)
+            else
+            {
+                Toast.makeText(this, "연결할 앱이 없습니다",
+                    Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
